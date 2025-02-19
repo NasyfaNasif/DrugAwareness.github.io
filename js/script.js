db86ef030b43cd7acce944f2b1085dfb9b31a300
NasyfaNasif <nasyfaan44@gmail.com>
2025-02-19 13:50:47 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
-menu = document .getElementById("menu");
+menu = document.getElementById("menu");
 mobile = document.getElementById("mobile");
 overlay = document.getElementById("overlay");
 
-menu.addEventListner("click", () => {
-    if (mobile.style.display == "none"){
-        mobile.style.display = "block"
-        overlay.style.display = "block"
-    }else {
-        mobile.style.display = "none"
-        overlay.style.sisplay = "none"
-    }
-}) 
-overlay.addEventListner("click", () => {
-    mobile.style.display = "none"
-    overlay.style.display = "none"
-})   
+menu.addEventListener("click", () => {  
+    if (mobile.style.display == "none") {
+        mobile.style.display = "block";
+        overlay.style.display = "block";
+    } else {
+        mobile.style.display = "none";
+        overlay.style.display = "none";  
+}});
+
+
+overlay.addEventListener("click", () => { 
+    mobile.style.display = "none";
+    overlay.style.display = "none";
+});
